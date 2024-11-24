879f3acb6b1b69bb0300937b5b7160d957c4da0b
unknown <isailandao@hotmail.com>
2024-11-23 19:46:20 -0600

    feat: apply custom theme with MUI ThemeProvider and CssBaseline

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,19 +1,25 @@
 // src/AppRouter.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import theme from './theme';
 import Register from './components/Register';
 import Login from './components/Login';
 import Profile from './components/Profile';
 
 function AppRouter() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Register />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/profile" element={<Profile />} />
-      </Routes>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
